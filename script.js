bb5d78660673ec776d07b2076c032e8f53d5f755
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2022-12-30 16:42:39 +0530

    added functionality to edit notes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 class App {
     constructor() {
         this.notes = [];
+        this.title = '';
+        this.text = '';
+        this.id = '';
+
         this.form = document.querySelector(".form");
         this.formTitle = document.querySelector(".form-title");
         this.formText = document.querySelector(".form-text");
@@ -9,12 +13,18 @@ class App {
         this.formButtonContainer = document.querySelector(".form-buttons-container");
         this.notesContainer = document.querySelector(".notes-container");
         this.closeBtn = document.querySelector("#close-btn");
+        this.modalContainer = document.querySelector(".modal-container");
+        this.modalTitle = document.querySelector(".modal-title")
+        this.modalText = document.querySelector(".modal-text")
+        this.modalCloseBtn = document.querySelector(".modal-close-btn");
         this.addEventListeners();
     }
 
     addEventListeners() {
         document.addEventListener("click", event => {
             this.handleFormClick(event);
+            this.selectNote(event);
+            this.openModal(event);
         })
 
         this.form.addEventListener("submit", event => {
@@ -31,6 +41,10 @@ class App {
         this.closeBtn.addEventListener("click", (event) => {
             event.stopPropagation();
             this.closeForm();
+        })
+
+        this.modalCloseBtn.addEventListener("click", (event) => {
+            this.closeModal();
         })
     }
 
@@ -70,16 +84,47 @@ class App {
         this.notes = [...this.notes, newNote];
         this.displayNote();
         this.closeForm();
-        // console.log(this.notes)
     }
 
     displayNote() {
         this.notesContainer.innerHTML = this.notes.map(item => `
-            <div class="note">
+            <div class="note" data-id="${item.id}">
                 <div class="note-title">${item.title}</div>
                 <div class="note-text">${item.text}</div>
             </div>
         `).join('')
+    }
+
+    selectNote(event) {
+        const selectedNote = event.target.closest(".note");
+        if (!selectedNote) return;
+        const [noteTitle, noteText] = selectedNote.children;
+        this.title = noteTitle.innerText;
+        this.text = noteText.innerText;
+        this.id = selectedNote.dataset.id;
+    }
+
+    openModal(event) {
+        if (event.target.closest(".note")) {
+            this.modalContainer.classList.toggle("open-modal");
+            this.modalTitle.value = this.title;
+            this.modalText.value = this.text;
+        }
+    }
+
+    closeModal(event) {
+        this.editModal();
+        this.modalContainer.classList.toggle("open-modal");
+    }
+
+    editModal() {
+        const title = this.modalTitle.value;
+        const text = this.modalText.value;
+
+        this.notes = this.notes.map(note =>
+            note.id === Number(this.id) ? { ...note, title, text } : note);
+
+        this.displayNote();
     }
 
 }
